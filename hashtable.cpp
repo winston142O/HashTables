7493cee5f33d3b3acd90f5d84beb4b4fae2f4c75
winton142O <winston1420@hotmail.com>
2023-10-02 17:49:00 +0000

    Introducción al programa

---
+++ hashtable.cpp
@@ -0,0 +1,30 @@
+/*
+    Hola bienvenido a este programa!
+    En este podran ver un ejemplo de la estructura de datos llamada Hash Table.
+
+    Este programa acepta un ID (su ID asignado por el INTEC) y ubique al estudiante en el índice
+    correspondiente. La estructura para este caso debe ser:
+    
+    La tabla hash está limitada a MAX (pero en este caso, estará limitada al número de 
+    estudiantes de su sección de Estructuras de Datos y Algoritmos), por
+    lo que solo aceptará los ID distintos de su clase y asignará la posición correspondiente.
+
+    Integrantes:
+        1116783-Whisman Lorenzo
+        1116842-Jose Reyes
+        1105307-Jose Sol
+        1114056-Jose Florentino
+        1116176-Manuel Guzman
+        1115997-Winston Pichardo
+        1087257-Jorge Jimenez
+
+        Fecha: 2/10/2023
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
